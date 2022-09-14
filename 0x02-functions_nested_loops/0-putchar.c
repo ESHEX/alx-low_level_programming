9a2090341b181a72402482f7516ecf578d63f7a2
ESHEX <miracleaneke@gmail.com>
2022-09-14 16:11:53 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
-#include "_putchar.h"
+#include "main.h"
+
 /**
  * main - Prints _putchar
- *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
+
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
